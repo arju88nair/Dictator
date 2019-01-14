7c5556e696f25d06b986bb27d44b61dfc8c99525
arju88nair <arju88nair@gmail.com>
2019-01-14 12:01:35 +0530

    summarizer

--- formatter.py
+++ formatter.py
@@ -1,106 +1,64 @@
-"""
-Simple-Summarizer
-A very simple summarizier built using NLP in Python
-https://jodylecompte.com
-https://github.com/jodylecompte/Simple-Summarizer
-"""
-import argparse
+import bs4 as bs  
+import urllib.request  
+import re
+from nltk.corpus import stopwords as sw
+from nltk import sent_tokenize
+import nltk 
+from nltk import word_tokenize
+import heapq  
 
-from nltk.tokenize import sent_tokenize, word_tokenize
-from nltk.corpus import stopwords
-from string import punctuation
-from nltk.probability import FreqDist
-from heapq import nlargest
-from collections import defaultdict
+# nltk.download('stopwords')
 
-def main():
-    """ Drive the process from argument to output """ 
-    args = parse_arguments()
+scraped_data = urllib.request.urlopen('https://en.wikipedia.org/wiki/Artificial_intelligence')  
+article = scraped_data.read()
 
-    content = read_file(args.filepath)
-    content = sanitize_input(content)
+parsed_article = bs.BeautifulSoup(article,'lxml')
 
-    sentence_tokens, word_tokens = tokenize_content(content)  
-    sentence_ranks = score_tokens(word_tokens, sentence_tokens)
+paragraphs = parsed_article.find_all('p')
 
-    return summarize(sentence_ranks, sentence_tokens, args.length)
+article_text = ""
 
-def parse_arguments():
-    """ Parse command line arguments """ 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('filepath', help='File name of text to summarize')
-    parser.add_argument('-l', '--length', default=4, help='Number of sentences to return')
-    args = parser.parse_args()
+for p in paragraphs:  
+    article_text += p.text
 
-    return args
+article_text = re.sub(r'\[[0-9]*\]', ' ', article_text)  
+article_text = re.sub(r'\s+', ' ', article_text)  
 
-def read_file(path):
-    """ Read the file at designated path and throw exception if unable to do so """ 
-    try:
-        with open(path, 'r') as file:
-            return file.read()
+# Removing special characters and digits
+formatted_article_text = re.sub('[^a-zA-Z]', ' ', article_text )  
+formatted_article_text = re.sub(r'\s+', ' ', formatted_article_text)  
 
-    except IOError as e:
-        print("Fatal Error: File ({}) could not be locaeted or is not readable.".format(path))
 
-def sanitize_input(data):
-    """ 
-    Currently just a whitespace remover. More thought will have to be given with how 
-    to handle sanitzation and encoding in a way that most text files can be successfully
-    parsed
-    """
-    replace = {
-        ord('\f') : ' ',
-        ord('\t') : ' ',
-        ord('\n') : ' ',
-        ord('\r') : None
-    }
+sentence_list = sent_tokenize(article_text)  
+stopwords = sw.words('english')
 
-    return data.translate(replace)
+word_frequencies = {}  
+for word in word_tokenize(formatted_article_text):  
+    if word not in stopwords:
+        if word not in word_frequencies.keys():
+            word_frequencies[word] = 1
+        else:
+            word_frequencies[word] += 1
 
-def tokenize_content(content):
-    """
-    Accept the content and produce a list of tokenized sentences, 
-    a list of tokenized words, and then a list of the tokenized words
-    with stop words built from NLTK corpus and Python string class filtred out. 
-    """
-    stop_words = set(stopwords.words('english') + list(punctuation))
-    words = word_tokenize(content.lower())
-    
-    return [
-        sent_tokenize(content),
-        [word for word in words if word not in stop_words]    
-    ]
 
-def score_tokens(filterd_words, sentence_tokens):
-    """
-    Builds a frequency map based on the filtered list of words and 
-    uses this to produce a map of each sentence and its total score
-    """
-    word_freq = FreqDist(filterd_words)
+maximum_frequncy = max(word_frequencies.values())
 
-    ranking = defaultdict(int)
+for word in word_frequencies.keys():  
+    word_frequencies[word] = (word_frequencies[word]/maximum_frequncy)
 
-    for i, sentence in enumerate(sentence_tokens):
-        for word in word_tokenize(sentence.lower()):
-            if word in word_freq:
-                ranking[i] += word_freq[word]
 
-    return ranking
+sentence_scores = {}  
+for sent in sentence_list:  
+    for word in word_tokenize(sent.lower()):
+        if word in word_frequencies.keys():
+            if len(sent.split(' ')) < 30:
+                if sent not in sentence_scores.keys():
+                    sentence_scores[sent] = word_frequencies[word]
+                else:
+                    sentence_scores[sent] += word_frequencies[word]
 
-def summarize(ranks, sentences, length):
-    """
-    Utilizes a ranking map produced by score_token to extract
-    the highest ranking sentences in order after converting from
-    array to string.  
-    """
-    if int(length) > len(sentences): 
-        print("Error, more sentences requested than available. Use --l (--length) flag to adjust.")
-        exit()
+summary_sentences = heapq.nlargest(7, sentence_scores, key=sentence_scores.get)
 
-    indexes = nlargest(length, ranks, key=ranks.get)
-    final_sentences = [sentences[j] for j in sorted(indexes)]
-    return ' '.join(final_sentences) 
+summary = ' '.join(summary_sentences)  
 
-if __name__ == "__main__":
-    print(main())
+print(summary)
